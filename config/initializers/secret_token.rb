a5a0f93634d03d8b5e6e4cabdc3750ca269185a1
giulia <g_l_89@libero.it>
2013-05-07 12:03:43 +0200

    ok..progetto vuoto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newdad::Application.config.secret_token = 'ab8d8b15391d65d606b864af40e8881c4053fa5443617bf92ee1217d857e34a444458b579c27f3e28bbc388acee97cf01ea044054c7578d436f9a0ce0dff7a59'
