2c0432ee6bb8ee0717ef0d781d22e0c9c394edad
mariateresag <mariateresa.gabbiadini@studenti.polito.it>
2013-07-10 20:31:15 +0200

    css

--- app/assets/javascripts/DD.js
+++ app/assets/javascripts/DD.js
@@ -1,5 +1,3 @@
-
-// dragDefine - called when an item starts to be dragged
 function dragDefine(ev) {
     ev.dataTransfer.effectAllowed = 'move';
     ev.dataTransfer.setData("text/plain", ev.target.getAttribute('id'));
@@ -7,29 +5,16 @@ function dragDefine(ev) {
     return true;
     }
 
-// dragOver - called when the item being dragged is over another item
 function dragOver(ev) {
-    // Get the id of the item being dragged
-    //var idDrag = ev.dataTransfer.getData("Text");
-    // Get the id of the item the dragged object is currently over
-    //var idBox = ev.target.getAttribute('id');
-    // You may want to perform some checks here, e.g. is the dragged item allowed to be dropped here for example
-
-    // Turn off the default behaviour i.e. allow the drop
     ev.preventDefault();
     }
 
-// dragEnd - called when the drag is complete
 function dragEnd(ev) {
     return true;
     }
 
-// dragDrop - called when the item being dragged is dropped
 function dragDrop(ev) {
-    // Get the id of the item being dragged
     var idDrag = ev.dataTransfer.getData("Text");
-    // Append the dragged item to the item it is currently over
     ev.target.appendChild(document.getElementById(idDrag));
-    // Turn off the default behaviour
     ev.preventDefault();
     }
