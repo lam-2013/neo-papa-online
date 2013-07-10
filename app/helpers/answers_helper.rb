c7a26cfd933400b9a0a43cacdc93a74fe1e1fb28
giulia <g_l_89@libero.it>
2013-07-11 01:22:12 +0200

    aggiustamenti visualizzazione della risposta nella home e nel profilo tolti gli id di category e age_group

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -2,8 +2,18 @@ module AnswersHelper
 
   def current_answer(answer)
     @current_answer = answer
-  end
+   end
+    def question(answer)
+      question_id = answer.question_id
+      @q = Question.find_by_id(question_id)
+    end
 
+  def user(answer)
+    question_id = answer.question_id
+    q = Question.find_by_id(question_id)
+    user_id = q.user_id
+    @u = User.find_by_id(user_id)
+  end
 
 
 end
